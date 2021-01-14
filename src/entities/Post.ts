7d0baf06df35bafa5af0f84316e6a497d6753417
satish-313 <pradhansatish53@gmail.com>
2021-01-14 19:45:58 +0530

    basic crud using graphql

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,18 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
-
+  @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
-  @Property({ type : "date"})
+  @Field(() => String)
+  @Property({ type: "date" })
   createdAt = new Date();
 
-  @Property({ type: "date",onUpdate: () => new Date() })
+  @Field(() => String)
+  @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
-  @Property({type: "text"})
+  @Field() // if we comment the field we can't expose to use data in graphql
+  @Property({ type: "text" })
   title!: string;
-
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
+import express from 'express'
+
+// db
 import {MikroORM} from '@mikro-orm/core'
 import { __prod__ } from './constants'
-import { Post } from './entities/Post'
 import microConfig from './mikro-orm.config'
+
+// apollo
+import "reflect-metadata"
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
+import { PostResolver } from './resolvers/post'
 
 const main = async() => {
   const orm = await MikroORM.init(microConfig)
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post,{title: 'my first post'})
-  // await orm.em.persistAndFlush(post)
+  
+  const app = express();
 
-  // const posts = await orm.em.find(Post,{})
-  // console.log(posts)
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver,PostResolver],
+      validate: false,
+    }),
+    context: () => ({em: orm.em})
+  });
+
+  apolloServer.applyMiddleware({app})
+
+  app.listen(4500, () => {
+    console.log('server is running on port : 4500')
+  })
 };
 
 main().catch((err) => {
